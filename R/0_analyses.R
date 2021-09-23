e6bb8e85f810148479c863bb34066de0ab38e573
Lewis Barnett <labarnett@ucdavis.edu>
2021-09-23 13:51:48 -0700

    update data file paths for 2021 production analysis

--- R/0_analyses.R
+++ R/0_analyses.R
@@ -17,13 +17,13 @@ get_data(channel = channel)
 # Writes:
 # -- /plots/RSPE_violin_GEAR_TEMPERATURE_[n].png: Plots of root square prediction error for interpolation methods.
 # -- /output/[date]_rmse_loocv_GEAR_TEMPERATURE_[year].csv: Results of leave-one-out cross validation.
-loocv_gear_temp(temp_data_path =  here::here("data", list.files(here::here("data"))[length(list.files(here::here("data")))])) # update file name manually if need specific data file date
+loocv_gear_temp(temp_data_path =  here::here("data", "2021-09-23_index_hauls_temperature_data.csv")) # update file name manually if need specific data file date
 
 # Calculate cold pool area using interpolation methods ----
 # Writes:
 # -- /output/raster/[method]_[year]_gear_temperature.tif: GeoTIFF raster files for each interpolation method and year
 # -- /output/estimate_cpa/cpa_out.csv: Cold pool areas by year for each interpolation method.
-interpolate_gear_temp(temp_data_path =  here::here("data", list.files(here::here("data"))[length(list.files(here::here("data")))]),
+interpolate_gear_temp(temp_data_path =  here::here("data", "2021-09-23_index_hauls_temperature_data.csv"),
                       proj_crs = "EPSG:3338",
                       cell_resolution = 5000)
 
@@ -35,4 +35,4 @@ source(here::here("R", "cpa_change.R"))
 #                           sel_old_raster = "./data/idw_files/bt00_idw/",
 #                           sel_idw4_geotiff = "./output/raster/idw_nmax4_2000_gear_temperature.tif",
 #                           set_crs = "EPSG:3338",
-#                           temp_data_path = here::here("data", list.files(here::here("data"))[length(list.files(here::here("data")))]))
+#                           temp_data_path = here::here("data", "2021-04-11_all_temperature_data.csv"))
