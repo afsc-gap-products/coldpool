6ffbddccb19e9a6788b01c8ba79b9478bef42247
sean-rohan-NOAA <sean.rohan@noaa.gov>
2021-09-27 13:44:48 -0700

    Clean up syntax

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Eastern Bering Sea Cold Pool Index
-_Sean Rohan_ and _Lewis Barnett_
+_Sean Rohan and Lewis Barnett_
 
 This repository contains a package that is used to calculate the Cold Pool Index for the eastern Bering Sea using bottom temperature data collected during AFSC bottom trawl surveys, where the cold pool index is defined as the area of the NOAA/AFSC/RACE eastern Bering Sea with bottom temperatures less than or equal to 2° Celsius, in square kilometers.
 
--- inst/scripts/include_sysdata.R
+++ inst/scripts/include_sysdata.R
@@ -1,13 +1,10 @@
-# Include cold pool index in coldpool R package
-# To be run after generating cold pool index.
+# Include cold pool index package 
+# Run after interpolating temperature and calculating cold pool area.
+library(coldpool)
 
 cpa_pre2021 <- read.csv(file = "./inst/extdata/cpa_areas2019.csv")
 ebs_proj_crs <- "EPSG:3338"
 cold_pool_index <- read.csv(file = "./output/estimated_cpa/cpa_out_ste.csv") %>%
   dplyr::rename(COLD_POOL_AREA_KM2 = ste_lte2, YEAR = year) %>%
   dplyr::mutate(LAST_UPDATE = Sys.Date())
-save(cpa_pre2021, ebs_proj_crs, cold_pool_index, file = "./data/sysdata.rda")
-rm(cpa_pre2021)
-rm(ebs_proj_crs)
-rm(cold_pool_index)
-load("./data/sysdata.rda")
+save(cpa_pre2021, ebs_proj_crs, cold_pool_index, file = "./R/sysdata.rda")
